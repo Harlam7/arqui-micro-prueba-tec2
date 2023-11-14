a7bec69e0373a0ba979096ba422ff0c2bf2b6b4c
Harlam7 <31184547+Harlam7@users.noreply.github.com>
2023-11-14 15:58:11 -0500

    update para findByClienteId

--- clientePersona-service/src/main/java/com/clientePersona/service/repositorios/ClienteRepository.java
+++ clientePersona-service/src/main/java/com/clientePersona/service/repositorios/ClienteRepository.java
@@ -4,5 +4,8 @@ package com.clientePersona.service.repositorios;
 import com.clientePersona.service.modelos.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Object> findByClienteId(Long aLong);
 }
